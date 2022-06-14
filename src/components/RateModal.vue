e7a193834a1304f0b9e78217664eb7950fb8766f
alxevvv <alxevvv@gmail.com>
2022-06-14 05:20:32 +0300

    rate modal percents field distribute remainders

--- src/components/RateModal.vue
+++ src/components/RateModal.vue
@@ -118,11 +118,13 @@
           <b-field class="column is-2">
             <b-input
               type="number"
-              step="0.01"
+              step="1"
+              min="0"
+              max="100"
               required
               v-model="columnPercent[idx]"
-              v-bind="{ readonly: true }"
               @focus="setLastTouchedField(row, 'percent')"
+              @keyup.native="() => distributePercentRemainders(true)"
             ></b-input>
             <p class="control">
               <b-button class="button is-static" icon-left="percent" />
@@ -142,12 +144,17 @@
         </div>
       </div>
       <div class="columns">
-        <b-field label="Calculated Rate" class="column is-4 is-offset-3">
+        <b-field
+          :label="`${
+            finalRateSource === 'current' ? 'Current' : 'Calculated'
+          } Rate`"
+          class="column is-4 is-offset-3"
+        >
           <b-input
             type="number"
             size="is-large"
             step="0.000000000000001"
-            v-bind="{ readonly: true }"
+            v-bind="{readonly: true}"
             v-model="finalRate"
           >
           </b-input>
@@ -174,9 +181,9 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import {mapGetters} from "vuex"
 
-const mapId = (obj) => obj.id;
+const mapId = (obj) => obj.id
 
 function prepareTransfers(initialTransfers) {
   return Object.freeze(
@@ -189,7 +196,7 @@ function prepareTransfers(initialTransfers) {
         amount: transfer.amountIn,
       }))
       .sort((a, b) => b.date.getTime() - a.date.getTime())
-  );
+  )
 }
 
 export default {
@@ -207,6 +214,7 @@ export default {
       transfersSelected: [],
       transfersCustom: [],
       finalRate: this.currentRate,
+      finalRateSource: "current",
       adaAmount: this.initialAdaAmount,
       usdAmount: this.initialUsdAmount,
       lastTouchedAmount: this.initialLastTouchedAmount,
@@ -218,7 +226,9 @@ export default {
       columnAdaAmount: [],
       columnUsdAmount: [],
       columnPercent: [],
-    };
+      columnPercentEdited: [],
+      columnPercentLocked: [],
+    }
   },
 
   computed: {
@@ -230,116 +240,141 @@ export default {
         this.columnAdaAmount,
         this.columnUsdAmount,
         this.columnPercent,
-      ];
+      ]
     },
 
     totalAmount() {
-      return this.columnAdaAmount.reduce((acc, cur) => (acc += +cur), 0);
+      return this.columnAdaAmount.reduce((acc, cur) => (acc += +cur), 0)
     },
   },
 
   watch: {
-    transfersSelected(transferIds) {
-      this.rows = [
-        ...transferIds.map((id) => ({ id, isCustom: false })),
-        ...this.rows.filter(({ isCustom }) => isCustom),
-      ];
+    transfersSelected(transferIds, previousTransferIds) {
+      const rows = [
+        ...transferIds.map((id) => ({id, isCustom: false})),
+        ...this.rows.filter(({isCustom}) => isCustom),
+      ]
+      this.rows = rows
+      if (transferIds.length < previousTransferIds.length) {
+        this.$nextTick(this.distributePercentRemainders)
+      } else if (transferIds.length > previousTransferIds.length) {
+        const addedTransferIds = transferIds.filter(
+          (id) => !previousTransferIds.includes(id)
+        )
+        for (const id of addedTransferIds) {
+          const idx = transferIds.indexOf(id)
+          this.columnPercent.splice(idx, 0, "")
+        }
+      }
     },
 
     transfersCustom(transferIds) {
       this.rows = [
-        ...this.rows.filter(({ isCustom }) => !isCustom),
-        ...transferIds.map((id) => ({ id, isCustom: true })),
-      ];
+        ...this.rows.filter(({isCustom}) => !isCustom),
+        ...transferIds.map((id) => ({id, isCustom: true})),
+      ]
     },
 
     rows(rows, oldRows) {
-      const rowIds = rows.map(mapId);
-      const oldRowIds = oldRows.map(mapId);
+      const rowIds = rows.map(mapId)
+      const oldRowIds = oldRows.map(mapId)
 
-      const columnDate = [...this.columnDate];
-      const columnRate = [...this.columnRate];
-      const columnAdaAmount = [...this.columnAdaAmount];
-      const columnUsdAmount = [...this.columnUsdAmount];
+      const columnDate = [...this.columnDate]
+      const columnRate = [...this.columnRate]
+      const columnAdaAmount = [...this.columnAdaAmount]
+      const columnUsdAmount = [...this.columnUsdAmount]
 
-      let insertIdxOffset = 0;
-      for (const [idx, { id, isCustom }] of rows.entries()) {
+      let insertIdxOffset = 0
+      for (const [idx, {id, isCustom}] of rows.entries()) {
         if (!oldRowIds.includes(id)) {
           if (isCustom) {
-            columnDate.splice(idx + insertIdxOffset, 0, new Date());
+            columnDate.splice(idx + insertIdxOffset, 0, new Date())
             columnRate.splice(
               idx + insertIdxOffset,
               0,
-              this.currentRate.toString()
-            );
-            columnAdaAmount.splice(idx + insertIdxOffset, 0, "");
-            columnUsdAmount.splice(idx + insertIdxOffset, 0, "");
+              this.currentRate.toString().slice(0, 17)
+            )
+            columnAdaAmount.splice(idx + insertIdxOffset, 0, "")
+            columnUsdAmount.splice(idx + insertIdxOffset, 0, "")
           } else {
-            const transfer = this.transfers.find((t) => t.id === id);
-            columnDate.splice(idx + insertIdxOffset, 0, transfer.date);
+            const transfer = this.transfers.find((t) => t.id === id)
+            columnDate.splice(idx + insertIdxOffset, 0, transfer.date)
             columnRate.splice(
               idx + insertIdxOffset,
               0,
               transfer.rate.toString()
-            );
+            )
             columnAdaAmount.splice(
               idx + insertIdxOffset,
               0,
               transfer.amount.toString()
-            );
+            )
             columnUsdAmount.splice(
               idx + insertIdxOffset,
               0,
               (transfer.amount * transfer.rate).toFixed(2)
-            );
+            )
           }
-          insertIdxOffset += 1;
+          insertIdxOffset += 1
         }
       }
 
-      let removeIdxOffset = 0;
-      for (const [idx, { id }] of oldRows.entries()) {
+      let removeIdxOffset = 0
+      for (const [idx, {id}] of oldRows.entries()) {
         if (!rowIds.includes(id)) {
-          columnDate.splice(idx + removeIdxOffset, 1);
-          columnRate.splice(idx + removeIdxOffset, 1);
-          columnAdaAmount.splice(idx + removeIdxOffset, 1);
-          columnUsdAmount.splice(idx + removeIdxOffset, 1);
-          removeIdxOffset -= 1;
+          columnDate.splice(idx + removeIdxOffset, 1)
+          columnRate.splice(idx + removeIdxOffset, 1)
+          columnAdaAmount.splice(idx + removeIdxOffset, 1)
+          columnUsdAmount.splice(idx + removeIdxOffset, 1)
+          removeIdxOffset -= 1
         }
       }
 
-      this.columnDate = columnDate;
-      this.columnRate = columnRate;
-      this.columnAdaAmount = columnAdaAmount;
-      this.columnUsdAmount = columnUsdAmount;
+      this.columnDate = columnDate
+      this.columnRate = columnRate
+      this.columnAdaAmount = columnAdaAmount
+      this.columnUsdAmount = columnUsdAmount
 
       this.rowsLastTouchedAmount = Object.fromEntries(
-        rows.map(({ id }) => [id, this.rowsLastTouchedAmount[id] || "ada"])
-      );
+        rows.map(({id}) => [id, this.rowsLastTouchedAmount[id] || "ada"])
+      )
+    },
+
+    columnRate(columnRate) {
+      if (
+        this.lastTouchedField &&
+        this.lastTouchedField.columnName === "rate"
+      ) {
+        const rowId = this.lastTouchedField.id
+        const rowIdx = this.rows.findIndex(({id}) => id === rowId)
+        const rate = +columnRate[rowIdx]
+        const rowLastTouchedAmount = this.rowsLastTouchedAmount[rowId] || "ada"
+        if (rowLastTouchedAmount === "ada") {
+          const columnUsdAmount = [...this.columnUsdAmount]
+          const usdAmount = +this.columnAdaAmount[rowIdx] * rate
+          columnUsdAmount[rowIdx] = usdAmount ? usdAmount.toFixed(2) : ""
+          this.columnUsdAmount = columnUsdAmount
+        } else {
+          const columnAdaAmount = [...this.columnAdaAmount]
+          const adaAmount = +this.columnUsdAmount[rowIdx] / rate
+          columnAdaAmount[rowIdx] = adaAmount ? adaAmount.toFixed(2) : ""
+          this.columnAdaAmount = columnAdaAmount
+        }
+      }
     },
 
     columnAdaAmount(columnAdaAmount) {
-      const columnPercent = columnAdaAmount.map((adaAmount) =>
-        ((1 / this.totalAmount) * +adaAmount * 100).toFixed(2)
-      );
-      columnPercent[columnPercent.length - 1] = (
-        Math.round(
-          columnPercent.slice(0, -1).reduce((acc, cur) => (acc -= +cur), 100) *
-            100
-        ) / 100
-      ).toFixed(2);
-      this.columnPercent = columnPercent;
       if (
         this.lastTouchedField &&
         this.lastTouchedField.columnName === "adaAmount"
       ) {
-        const columnUsdAmount = [...this.columnUsdAmount];
-        const rowId = this.lastTouchedField.id;
-        const rowIdx = this.rows.findIndex(({ id }) => id === rowId);
-        const rate = +this.columnRate[rowIdx];
-        const usdAmount = +columnAdaAmount[rowIdx] * rate;
-        columnUsdAmount[rowIdx] = usdAmount ? usdAmount.toFixed(2) : "";
-        this.columnUsdAmount = columnUsdAmount;
+        const columnUsdAmount = [...this.columnUsdAmount]
+        const rowId = this.lastTouchedField.id
+        const rowIdx = this.rows.findIndex(({id}) => id === rowId)
+        const rate = +this.columnRate[rowIdx]
+        const usdAmount = +columnAdaAmount[rowIdx] * rate
+        columnUsdAmount[rowIdx] = usdAmount ? usdAmount.toFixed(2) : ""
+        this.columnUsdAmount = columnUsdAmount
       }
     },
 
@@ -348,129 +383,180 @@ export default {
         this.lastTouchedField &&
         this.lastTouchedField.columnName === "usdAmount"
       ) {
-        const columnAdaAmount = [...this.columnAdaAmount];
-        const rowId = this.lastTouchedField.id;
-        const rowIdx = this.rows.findIndex(({ id }) => id === rowId);
-        const rate = +this.columnRate[rowIdx];
-        const adaAmount = +columnUsdAmount[rowIdx] / rate;
-        columnAdaAmount[rowIdx] = adaAmount ? adaAmount.toFixed(2) : "";
-        this.columnAdaAmount = columnAdaAmount;
-      }
-    },
-
-    columnRate(columnRate) {
-      if (
-        this.lastTouchedField &&
-        this.lastTouchedField.columnName === "rate"
-      ) {
-        const rowId = this.lastTouchedField.id;
-        const rowIdx = this.rows.findIndex(({ id }) => id === rowId);
-        const rate = +columnRate[rowIdx];
-        const rowLastTouchedAmount = this.rowsLastTouchedAmount[rowId] || "ada";
-        if (rowLastTouchedAmount === "ada") {
-          const columnUsdAmount = [...this.columnUsdAmount];
-          const usdAmount = +this.columnAdaAmount[rowIdx] * rate;
-          columnUsdAmount[rowIdx] = usdAmount ? usdAmount.toFixed(2) : "";
-          this.columnUsdAmount = columnUsdAmount;
-        } else {
-          const columnAdaAmount = [...this.columnAdaAmount];
-          const adaAmount = +this.columnUsdAmount[rowIdx] / rate;
-          columnAdaAmount[rowIdx] = adaAmount ? adaAmount.toFixed(2) : "";
-          this.columnAdaAmount = columnAdaAmount;
-        }
+        const columnAdaAmount = [...this.columnAdaAmount]
+        const rowId = this.lastTouchedField.id
+        const rowIdx = this.rows.findIndex(({id}) => id === rowId)
+        const rate = +this.columnRate[rowIdx]
+        const adaAmount = +columnUsdAmount[rowIdx] / rate
+        columnAdaAmount[rowIdx] = adaAmount ? adaAmount.toFixed(2) : ""
+        this.columnAdaAmount = columnAdaAmount
       }
     },
 
     columns(columns) {
-      const [columnRate, , , columnPercent] = columns;
-      if (columnRate.length === columnPercent.length) {
-        this.finalRate = (
-          Array.from(Array(columnRate.length).keys()).reduce(
-            (acc, idx) => (acc += columnRate[idx] * (columnPercent[idx] / 100)),
-            0
-          ) || this.currentRate
+      const [columnRate, , , columnPercent] = columns
+      if (
+        columnPercent.reduce((acc, cur) => (acc += parseFloat(cur)), 0) === 100
+      ) {
+        const finalRate = Array.from(Array(columnRate.length).keys()).reduce(
+          (acc, idx) => (acc += columnRate[idx] * (columnPercent[idx] / 100)),
+          0
         )
-          .toString()
-          .slice(0, 17);
+        if (finalRate) {
+          this.finalRate = finalRate.toString().slice(0, 17)
+          this.finalRateSource = "splits"
+          return
+        }
       }
+      this.finalRate = this.currentRate.toString().slice(0, 17)
+      this.finalRateSource = "current"
     },
 
     finalRate(finalRate) {
-      const rate = parseFloat(finalRate);
+      const rate = parseFloat(finalRate)
       if (this.lastTouchedAmount === "ada") {
-        const adaAmount = parseFloat(this.adaAmount);
-        let usdAmount = "";
+        const adaAmount = parseFloat(this.adaAmount)
+        let usdAmount = ""
         if (adaAmount === 0) {
-          usdAmount = 0;
+          usdAmount = 0
         } else if (adaAmount) {
-          usdAmount = parseFloat((parseFloat(adaAmount) * rate).toFixed(2));
+          usdAmount = parseFloat((parseFloat(adaAmount) * rate).toFixed(2))
         }
-        this.usdAmount = usdAmount.toString();
+        this.usdAmount = usdAmount.toString()
       } else {
-        const usdAmount = parseFloat(this.usdAmount);
-        let adaAmount = "";
+        const usdAmount = parseFloat(this.usdAmount)
+        let adaAmount = ""
         if (usdAmount === 0) {
-          adaAmount = 0;
+          adaAmount = 0
         } else if (usdAmount) {
-          adaAmount = parseFloat((parseFloat(usdAmount) / rate).toFixed(2));
+          adaAmount = parseFloat((parseFloat(usdAmount) / rate).toFixed(2))
         }
-        this.adaAmount = adaAmount.toString();
+        this.adaAmount = adaAmount.toString()
       }
     },
   },
 
   methods: {
     adjustAmountFromAda(e) {
-      const rate = this.finalRate || 1;
-      const value = parseFloat(e.target.value);
-      let usdAmount = "";
+      const rate = this.finalRate || 1
+      const value = parseFloat(e.target.value)
+      let usdAmount = ""
       if (value === 0) {
-        usdAmount = 0;
+        usdAmount = 0
       } else if (value) {
-        usdAmount = parseFloat((parseFloat(value) * rate).toFixed(2));
+        usdAmount = parseFloat((parseFloat(value) * rate).toFixed(2))
       }
-      this.usdAmount = usdAmount.toString();
-      this.lastTouchedAmount = "ada";
+      this.usdAmount = usdAmount.toString()
+      this.lastTouchedAmount = "ada"
     },
 
     adjustAmountFromUSD(e) {
-      const rate = this.finalRate || 1;
-      const value = parseFloat(e.target.value);
-      let adaAmount = "";
+      const rate = this.finalRate || 1
+      const value = parseFloat(e.target.value)
+      let adaAmount = ""
       if (value === 0) {
-        adaAmount = 0;
+        adaAmount = 0
       } else if (value) {
-        adaAmount = parseFloat((parseFloat(value) / rate).toFixed(2));
+        adaAmount = parseFloat((parseFloat(value) / rate).toFixed(2))
       }
-      this.adaAmount = adaAmount.toString();
-      this.lastTouchedAmount = "usd";
+      this.adaAmount = adaAmount.toString()
+      this.lastTouchedAmount = "usd"
     },
 
-    setLastTouchedField({ id }, columnName) {
-      this.lastTouchedField = { id, columnName };
+    setLastTouchedField({id}, columnName) {
+      this.lastTouchedField = {id, columnName}
       if (columnName.includes("Amount")) {
-        this.rowsLastTouchedAmount[id] = columnName.split("Amount")[0];
+        this.rowsLastTouchedAmount[id] = columnName.split("Amount")[0]
       }
+    },
+
+    distributePercentRemainders(fromField = false) {
+      const rowId = fromField ? this.lastTouchedField.id : null
+      const columnPercentEdited = [...this.columnPercentEdited]
+      if (columnPercentEdited.length === this.rows.length) {
+        if (columnPercentEdited.shift() === rowId) {
+          columnPercentEdited.shift()
+        }
+      }
+      if (rowId && !columnPercentEdited.includes(rowId)) {
+        columnPercentEdited.push(rowId)
+      }
+      const distributedIds = this.rows
+        .filter(({id}) => columnPercentEdited.includes(id))
+        .map(({id}) => id)
+      const distributionTargetsIds = this.rows
+        .filter(({id}) => !columnPercentEdited.includes(id))
+        .map(({id}) => id)
+      const distributedIndexes = distributedIds.map((rowId) =>
+        this.rows.findIndex(({id}) => id === rowId)
+      )
+      const distributionTargetsIndexes = distributionTargetsIds.map((rowId) =>
+        this.rows.findIndex(({id}) => id === rowId)
+      )
+      const alreadyDistributedSum = distributedIndexes.reduce(
+        (acc, cur) => (acc += parseFloat(this.columnPercent[cur] || 0)),
+        0
+      )
+      const restForDistribute = 100 - alreadyDistributedSum
+      const distributionPortion = Math.round(
+        restForDistribute / distributionTargetsIds.length
+      )
+      const distributions = this.rows.map((row) => {
+        if (distributionTargetsIds.includes(row.id)) {
+          return distributionPortion
+        }
+      })
+      const distributionsSum = distributions.reduce(
+        (acc, cur) => (acc += cur || 0),
+        0
+      )
+      const sum = alreadyDistributedSum + distributionsSum
+      if (sum > 100) {
+        distributions[
+          distributionTargetsIndexes[distributionTargetsIndexes.length - 1]
+        ] -= sum - 100
+      } else if (sum < 100) {
+        distributions[
+          distributionTargetsIndexes[distributionTargetsIndexes.length - 1]
+        ] += 100 - sum
+      }
+      const columnPercent = this.rows.map((row, idx) =>
+        distributions[idx] === undefined
+          ? this.columnPercent[idx]
+          : distributions[idx] > 0
+          ? distributions[idx].toString()
+          : "0"
+      )
+      this.columnPercent = columnPercent
+      this.columnPercentEdited = columnPercentEdited
     },
 
     addCustomRow() {
-      this.transfersCustom.push(Date.now().toString());
+      this.transfersCustom.push(Date.now().toString())
     },
 
     removeRow(id) {
       if (this.transfersSelected.includes(id)) {
-        this.transfersSelected.splice(this.transfersSelected.indexOf(id), 1);
+        this.transfersSelected.splice(this.transfersSelected.indexOf(id), 1)
       } else {
-        this.transfersCustom.splice(this.transfersCustom.indexOf(id), 1);
+        this.transfersCustom.splice(this.transfersCustom.indexOf(id), 1)
       }
+      console.log(id, this.columnPercentEdited)
+      this.columnPercentEdited = this.columnPercentEdited.filter(
+        (rowId) => id !== rowId
+      )
+      this.columnPercentLocked = this.columnPercentLocked.filter(
+        (rowId) => id !== rowId
+      )
+      this.$nextTick(this.distributePercentRemainders)
     },
   },
 
   mounted() {
-    const transfers = prepareTransfers(this.initialTransfers);
-    const transfersSelected = transfers.slice(0, 5).map(mapId);
-    this.transfers = transfers;
-    this.transfersSelected = transfersSelected;
+    const transfers = prepareTransfers(this.initialTransfers)
+    const transfersSelected = transfers.slice(0, 5).map(mapId)
+    this.transfers = transfers
+    this.transfersSelected = transfersSelected
   },
-};
+}
 </script>
