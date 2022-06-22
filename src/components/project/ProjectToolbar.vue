8dccf5baf33175fa139d6fe33002a14ead2b4c57
alxevvv <alxevvv@gmail.com>
2022-06-22 21:13:07 +0300

    project toolbar

---
+++ src/components/project/ProjectToolbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="project-toolbar columns">
+    <div class="column is-6">
+      <div class="buttons">
+        <b-button>New project</b-button>
+        <b-button icon-left="cog"></b-button>
+      </div>
+    </div>
+    <div class="column is-6">
+      <div class="buttons is-right">
+        <b-button>Export to .csv</b-button>
+        <b-button>Import from .csv</b-button>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="project-root" class="section container">
     <h1 class="title is-1">AIM Dispersal Tool</h1>
+    <ProjectToolbar />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import ProjectToolbar from "@/components/project/ProjectToolbar.vue";
 
 export default Vue.extend({
   name: "ProjectView",
+  components: {
+    ProjectToolbar,
+  },
 });
 </script>
