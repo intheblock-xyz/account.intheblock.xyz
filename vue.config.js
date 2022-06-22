f0ad1f0876a46d4cc5b8997d623ee00d59897172
alxevvv <alxevvv@gmail.com>
2022-06-22 15:33:58 +0300

    change title and favicon at index.html

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  pages: {
+    index: {
+      entry: "src/main.ts",
+      title: "AIM Dispersal Tool",
+    },
+  },
 });
