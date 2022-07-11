dec66e794ba358016eb5174ac5069b21b5177c8e
alxevvv <alxevvv@gmail.com>
2022-07-11 14:16:17 +0300

    preferences modal one component

--- src/components/layout/PreferencesModal.vue
+++ src/components/layout/PreferencesModal.vue
@@ -1,33 +1,48 @@
 <template>
-  <div class="modal-card">
-    <header class="modal-card-head">
-      <p class="modal-card-title">Project preferences</p>
-      <button type="button" class="delete" @click="$emit('close')" />
-    </header>
-    <section class="modal-card-body">
-      <b-field label="Project Title">
-        <b-input
-          v-model="projectTitle"
-          placeholder="[untitled project]"
-          ref="projectTitleInput"
-        >
-        </b-input>
-      </b-field>
-    </section>
-    <footer class="modal-card-foot">
-      <b-button
-        icon-left="check"
-        label="Apply"
-        @click="$emit('submit', { projectTitle })"
-        type="is-primary"
-      />
-      <b-button label="Cancel" @click="$emit('close')" />
-    </footer>
-  </div>
+  <b-modal
+    aria-modal
+    trap-focus
+    has-modal-card
+    destroy-on-hide
+    aria-role="dialog"
+    aria-label="Project preferences modal"
+    close-button-aria-label="Close"
+    :active="isPreferencesModalActive"
+    :on-cancel="hidePreferencesModal"
+  >
+    <template>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Project preferences</p>
+          <button type="button" class="delete" @click="hidePreferencesModal" />
+        </header>
+        <section class="modal-card-body">
+          <b-field label="Project Title">
+            <b-input
+              v-model="projectTitle"
+              placeholder="[untitled project]"
+              ref="projectTitleInput"
+            >
+            </b-input>
+          </b-field>
+        </section>
+        <footer class="modal-card-foot">
+          <b-button
+            icon-left="check"
+            label="Apply"
+            @click="submitPreferencesForm({ projectTitle })"
+            type="is-primary"
+          />
+          <b-button label="Cancel" @click="hidePreferencesModal" />
+        </footer>
+      </div>
+    </template>
+  </b-modal>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { mapGetters } from "vuex";
 
 export default Vue.extend({
   name: "ProjectTitle",
@@ -38,13 +53,36 @@ export default Vue.extend({
     };
   },
 
-  mounted() {
-    if (this.$refs.projectTitleInput) {
-      const input = this.$refs.projectTitleInput as HTMLInputElement;
-      if (typeof input.focus === "function") {
-        input.focus();
+  computed: {
+    ...mapGetters("ui", ["isPreferencesModalActive"]),
+  },
+
+  methods: {
+    hidePreferencesModal() {
+      this.$store.dispatch("ui/hidePreferencesModal");
+    },
+
+    submitPreferencesForm(formData: Record<string, unknown>) {
+      const { projectTitle } = formData;
+      this.$store.commit("project/setTitle", projectTitle);
+      this.hidePreferencesModal();
+    },
+  },
+
+  watch: {
+    isPreferencesModalActive(isPreferencesModalActive) {
+      if (isPreferencesModalActive) {
+        this.projectTitle = this.$store.state.project.title;
+        this.$nextTick(() => {
+          if (this.$refs.projectTitleInput) {
+            const input = this.$refs.projectTitleInput as HTMLInputElement;
+            if (typeof input.focus === "function") {
+              input.focus();
+            }
+          }
+        });
       }
-    }
+    },
   },
 });
 </script>
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -2,24 +2,7 @@
   <div id="project-root" class="section container">
     <h1 class="title is-1">AIM Dispersal Tool</h1>
     <ProjectToolbar />
-    <b-modal
-      aria-modal
-      trap-focus
-      has-modal-card
-      destroy-on-hide
-      aria-role="dialog"
-      aria-label="Project preferences modal"
-      close-button-aria-label="Close"
-      :active="isPreferencesModalActive"
-      :on-cancel="hidePreferencesModal"
-    >
-      <template>
-        <PreferencesModal
-          @close="hidePreferencesModal"
-          @submit="submitPreferencesForm"
-        ></PreferencesModal>
-      </template>
-    </b-modal>
+    <PreferencesModal />
     <ProjectTitle />
     <hr />
     <ProjectActions />
@@ -28,7 +11,6 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { mapGetters } from "vuex";
 import PreferencesModal from "@/components/layout/PreferencesModal.vue";
 import ProjectActions from "@/components/project/ProjectActions.vue";
 import ProjectTitle from "@/components/project/ProjectTitle.vue";
@@ -42,22 +24,6 @@ export default Vue.extend({
     ProjectActions,
     ProjectTitle,
     ProjectToolbar,
-  },
-
-  computed: {
-    ...mapGetters("ui", ["isPreferencesModalActive"]),
-  },
-
-  methods: {
-    hidePreferencesModal() {
-      this.$store.dispatch("ui/hidePreferencesModal");
-    },
-
-    submitPreferencesForm(formData: Record<string, unknown>) {
-      const { projectTitle } = formData;
-      this.$store.commit("project/setTitle", projectTitle);
-      this.hidePreferencesModal();
-    },
   },
 });
 </script>
